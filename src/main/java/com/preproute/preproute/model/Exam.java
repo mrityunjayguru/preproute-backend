7bd977e9e2a2d407c96bfde86e9172c6d9c6f5a0
nirajniit <niit.delhi.ind@gmail.com>
2025-09-23 16:15:36 +0530

    commit

---
+++ src/main/java/com/preproute/preproute/model/Exam.java
@@ -0,0 +1,26 @@
+package com.preproute.preproute.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "exam")
+public class Exam {
+	  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long examid;
+    private String title;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
---
+++ src/main/java/com/preproute/preproute/model/Question.java
@@ -0,0 +1,25 @@
+package com.preproute.preproute.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String correctAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "examid")
+    private Exam exam;
+}
