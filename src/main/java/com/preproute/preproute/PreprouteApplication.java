63f91baed72e1147abfe102c3603225f4735fe41
nirajniit <niit.delhi.ind@gmail.com>
2025-09-24 08:01:38 +0530

    committed as on 24-09-2025

--- src/main/java/com/preproute/preproute/PreprouteApplication.java
+++ src/main/java/com/preproute/preproute/PreprouteApplication.java
@@ -2,8 +2,27 @@ package com.preproute.preproute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
+@ServletComponentScan
 @SpringBootApplication
+@ComponentScan({"com.preproute.preproute"})
+@EntityScan("com.preproute.preproute.*")
+@EnableJpaRepositories("com.preproute.preproute.*")
+@EnableWebMvc
+/*@EnableConfigurationProperties({
+    FileStorageProperties.class
+})*/
+
+
 public class PreprouteApplication {
 
 	public static void main(String[] args) {
